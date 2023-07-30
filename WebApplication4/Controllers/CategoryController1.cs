62ea31249173acbaf4a5a4e7750c80bc65c38c65
Kaushik <kaushikgoud504@gmail.com>
2023-07-30 16:42:16 +0530

    Add project files.

---
+++ WebApplication4/Controllers/CategoryController1.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebApplication4.Data;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class CategoryController1 : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController1(ApplicationDbContext db)
+        {
+                
+            _db = db;   
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList= _db.Categories;
+             return View(objCategoryList);
+
+        }
+        public IActionResult Create()
+        {
+           
+            return View();
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+
+             _db.Categories.Add(obj);
+             _db.SaveChanges();
+             return RedirectToAction("Index");
+
+
+        }
+    }
+}
---
+++ WebApplication4/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication4.Models;
+
+namespace WebApplication4.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
